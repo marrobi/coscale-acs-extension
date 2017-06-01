d249d7edf97821b3cac06cd50ab6ab361e343ca2
Bruno <elbuen@brus.mx>
2017-06-01 11:07:48 +0200

    Added downloading of the others scripts

--- scripts/setupCoscaleK8s.sh
+++ scripts/setupCoscaleK8s.sh
@@ -1,1 +1,22 @@
-./installCoscale.sh
+#!/bin/bash
+
+# Service princaple details
+SP_NAME=$1
+SP_PASS=$2
+SP_TENANT=$3
+
+# Azure configuration
+RG_NAME=$4
+DC_LOCATION=$5
+
+# Kube cluster configuration
+KUBERNETES_CLUSTER_NAME=$6
+KUBERNETES_CLUSTER_DNS=$7
+
+# CoScale config
+COSCALE_USERNAME=$8
+COSCALE_PASSWORD=$9
+
+curl https://raw.githubusercontent.com/marrobi/coscale-acs-extension/master/scripts/createCluster.sh -o createCluster.sh
+curl https://raw.githubusercontent.com/marrobi/coscale-acs-extension/master/scripts/installCoscale.sh -o installCoscale.sh
+
