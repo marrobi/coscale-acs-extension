8c0104c0b97ab8e261c564628e5009e847f4699e
Bruno <elbuen@brus.mx>
2017-06-01 11:42:58 +0200

    first version to try

--- scripts/setupCoscaleK8s.sh
+++ scripts/setupCoscaleK8s.sh
@@ -14,9 +14,22 @@ KUBERNETES_CLUSTER_NAME=$6
 KUBERNETES_CLUSTER_DNS=$7
 
 # CoScale config
-COSCALE_USERNAME=$8
-COSCALE_PASSWORD=$9
+CS_USER=$8
+CS_PASS=$9
 
+DIRECTORY=scripts
+if ! [ -d "$DIRECTORY" ]; then
+    makedir DIRECTORY
+fi
+cd DIRECTORY
 curl https://raw.githubusercontent.com/marrobi/coscale-acs-extension/master/scripts/createCluster.sh -o createCluster.sh
 curl https://raw.githubusercontent.com/marrobi/coscale-acs-extension/master/scripts/installCoscale.sh -o installCoscale.sh
 
+echo "--- Running createCluster.sh Date is: `date` ---"
+echo "`/bin/bash ./createCluster.sh $SP_NAME $SP_PASS $SP_TENANT $RG_NAME $DC_LOCATION $KUBERNETES_CLUSTER_NAME $KUBERNETES_CLUSTER_DNS`" 
+echo "--- Finished createCluster.sh ---"
+
+
+echo "--- Running installCoscale.sh Date is: `date` ---"
+echo "`/bin/bash ./installCoscale.sh $CS_USER $CS_PASS`" 
+echo "--- Finished installCoscale.sh ---"
